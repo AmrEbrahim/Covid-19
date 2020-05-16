ffd252eba954d1e6640e9b711b2603cffc19927d
amr ebrahim <amro_156@hotmail.com>
2020-05-16 17:03:24 +0200

    edit statisticsTable style

--- src/component/StatisticsTable.js
+++ src/component/StatisticsTable.js
@@ -1,9 +1,75 @@
-import React, { useEffect, useState } from "react";
-import { Table } from "reactstrap";
+import React from "react";
 import { connect } from "react-redux";
-import { code, name } from "country-emoji";
 
 import { fetchCountryData } from "../actions";
+import { MDBDataTable } from "mdbreact";
+
+const StatisticsTable = ({ fetchCountryData, CountryData }) => {
+  const data = {
+    columns: [
+      {
+        label: "Flag",
+        field: "countryInfo.flag",
+        sort: "asc",
+      },
+      {
+        label: "Country",
+        field: "country",
+        sort: "asc",
+      },
+      {
+        label: "Continent",
+        field: "continent",
+        sort: "asc",
+      },
+      {
+        label: "Total Cases",
+        field: "cases",
+        sort: "asc",
+      },
+      {
+        label: "Active",
+        field: "active",
+        sort: "asc",
+      },
+      {
+        label: "Deaths",
+        field: "deaths",
+        sort: "asc",
+      },
+      {
+        label: "Recovered",
+        field: "recovered",
+        sort: "asc",
+      },
+    ],
+    rows: Object.values(CountryData),
+  };
+  return (
+    <div className="firstColor col-12 pt-5 pb-2 px-5 mb-2 table">
+      <MDBDataTable
+        paginationLabel={["←", "→"]}
+        entries={10}
+        pagesAmount={5}
+        // style={{ color: "white" }}
+        responsiveSm
+        hover
+        data={data}
+      />
+    </div>
+  );
+};
+const mapStateToProps = ({ CountryData }) => {
+  return {
+    CountryData,
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchCountryData,
+})(StatisticsTable);
+
+/*
 
 const StatisticsTable = ({ fetchCountryData, CountryData }) => {
   const [sortType, setSortType] = useState("asc");
@@ -124,3 +190,4 @@ const mapStateToProps = ({ CountryData }) => {
 export default connect(mapStateToProps, {
   fetchCountryData,
 })(StatisticsTable);
+*/
