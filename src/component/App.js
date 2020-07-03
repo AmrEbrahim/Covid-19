53c2dd5e3f437f523024f93188cba59b0ad60fe7
Amr Ebrahim <amro_156@hotmail.com>
2020-07-03 23:04:35 +0200

    adding notFound component

--- src/component/App.js
+++ src/component/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import World from "./World";
 import Country from "./Country";
+import NotFound from "./NotFound";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -17,6 +18,7 @@ const App = () => {
         <Header />
         <Route path="/" exact component={World}></Route>
         <Route path="/:id" exact component={Country}></Route>
+        <Route path="/country/NotFound" exact component={NotFound}></Route>
       </Router>
     </div>
   );
---
+++ src/component/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="notFound d-flex justfiy-content-center align-items-center h-100 w-100">
+        <img src="/not_found.png" alt="notfound" />
+      </div>
+    );
+  }
+}
+export default NotFound;
