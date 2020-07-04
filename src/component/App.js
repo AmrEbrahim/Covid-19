d72cf34b0df79bb51430d42b56146d02b441fb2e
Khaled Saad <elgendy1204@gmail.com>
2020-07-04 18:08:18 +0200

    Fix not found page

--- src/component/App.js
+++ src/component/App.js
@@ -1,27 +1,29 @@
-import React from "react";
-import Header from "./Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import World from "./World";
-import Country from "./Country";
-import NotFound from "./NotFound";
+import World from './World';
+import Country from './Country';
+import NotFound from './NotFound';
 
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-import "./App.css";
-import "jvectormap-next/jquery-jvectormap.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './App.css';
+import 'jvectormap-next/jquery-jvectormap.css';
 
 const App = () => {
-  return (
-    <div className="fluid-container">
-      <Router>
-        <Header />
-        <Route path="/" exact component={World}></Route>
-        <Route path="/:id" exact component={Country}></Route>
-        <Route path="/country/NotFound" exact component={NotFound}></Route>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="fluid-container">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={World}></Route>
+                    <Route path="/NotFound" exact component={NotFound}></Route>
+                    <Route path="/:id" component={Country}></Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
