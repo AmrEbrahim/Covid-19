505a3bd71d414ea13c56145cc36b8388b14ad8f5
Amr Ebrahim <amro_156@hotmail.com>
2020-07-04 19:27:22 +0200

    edit notFound component styles

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 class NotFound extends Component {
   render() {
     return (
-      <div className="notFound d-flex justfiy-content-center align-items-center h-100 w-100">
-        <img src="/not_found.png" alt="notfound" />
+      <div className="notFound d-flex flex-column justify-content-center align-items-center h-70 w-70">
+        <p className="mt-4 px-2 h4 text-center">Page Not Found</p>
+        <p className="px-2 h5 text-center">
+          Sorry We couldn't find this country
+        </p>
+        <img className="img-fluid px-2" src="/not_found.png" alt="notfound" />
       </div>
     );
   }
